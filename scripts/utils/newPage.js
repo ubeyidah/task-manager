6db7bf2811d2c2529f2cd44158e2cf93801d56ed
ubeyid oumer <ubeyidah@gmail.com>
2024-04-08 11:07:09 +0300

    new page for the add todo and update

---
+++ scripts/utils/newPage.js
@@ -0,0 +1,14 @@
+export class NewPage {
+  render(html) {
+    document.querySelector(
+      ".js-new-page-container"
+    ).innerHTML = `<div class="js-new-page new-page">${html}</div>`;
+  }
+  close() {
+    document.querySelector(".js-new-page").remove();
+  }
+  open() {
+    document.querySelector(".js-new-page").classList.add("open");
+  }
+}
+console.log(8);
